4e210c46257f7d01bcf038f90435fe18c8df30d4
dileepsaivegi <91449667+dileepsaivegi@users.noreply.github.com>
2023-02-02 21:47:21 +0530

    intial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import emojipedia from "../emojipedia";
+import Template from "./Template";
+
+function createMap(props) {
+  return (
+    <div>
+      <Template
+        key={props.id}
+        emoji={props.emoji}
+        name={props.name}
+        meaning={props.meaning}
+      />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {emojipedia.map(createMap)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Template.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Template(props) {
+  return (
+    <div>
+      <dl className="dictionary">
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label={props.name}>
+              {props.emoji}
+            </span>
+            <span>{props.name}</span>
+          </dt>
+          <dd>{props.meaning}</dd>
+        </div>
+      </dl>
+    </div>
+  );
+}
+
+export default Template;
